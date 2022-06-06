13c53f4174f752d9e75ddc97d7e6bc17dfdcb3a7
Dani <d.tarazoma@geekshubs.com>
2022-06-06 17:20:25 +0200

    feat: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Welcome(props) {
+  return <div><h1>{props.myText}</h1></div>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Welcome myText="Welcome to tasks app"/>
+       <Welcome myText="Enjoy with my tasks app"/>
       </header>
     </div>
   );
