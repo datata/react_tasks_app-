e437f071b4218371802290ab7d37eb16963c607c
Dani <d.tarazoma@geekshubs.com>
2022-06-08 16:29:38 +0200

    fix: solve css body

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
 import "./header.css";
 
-export default function Header() {
+export default function Header({userLogged}) {
+
     return (
         <div>
             <header>
                 <div className="container">
                     <h1><Link to="/">Tasks app</Link></h1>
                     <nav>
-                        <ul>
-                            <li><Link to="/login">Login</Link></li>
-                            <li><Link to="/register">Register</Link></li>
-                        </ul>
+                        {userLogged ?
+                            <ul> <li><Link to="/logout">Logout</Link></li></ul>
+                            : <ul>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/register">Register</Link></li>
+                            </ul>
+                        }
                     </nav>
                 </div>
             </header>
